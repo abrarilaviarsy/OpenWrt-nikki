4911f597fa21dfcd885bdd8359f5aa0b63751fa2
abrarilaviarsy <83096795+abrarilaviarsy@users.noreply.github.com>
2025-03-09 05:19:49 +0700

    Update app.js

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
@@ -35,7 +35,7 @@ return view.extend({
 
         let m, s, o;
 
-        m = new form.Map('nikki', _('Nikki'), `${_('Transparent Proxy with Mihomo on OpenWrt.')} <a href="https://github.com/nikkinikki-org/OpenWrt-nikki/wiki" target="_blank">${_('How To Use')}</a>`);
+        m = new form.Map('nikki');
 
         s = m.section(form.NamedSection, 'status', 'status', _('Status'));
 
